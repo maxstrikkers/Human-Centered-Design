5ba7dba83006887153d33a77ab8464ee458e678d
maxstrikkers <maxstrikkers@outlook.com>
2024-04-07 16:56:51 +0200

    Spul werkend

---
+++ functies.js
@@ -0,0 +1,12 @@
+const fs = require('fs').promises;
+
+async function getClothesData(fileName) {
+    const dataUrl = './public/data/' + fileName + '.json';
+    const data = await fs.readFile(dataUrl, 'utf-8');
+    return JSON.parse(data);
+}
+
+// EXPORTS ALL OF THE FUNCTIONS
+module.exports = {
+    getClothesData
+};
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const { Liquid } = require('liquidjs');
 const app = express();
 
+const { getClothesData } = require('./functies.js');
+
 const engine = new Liquid({
     root: path.resolve(__dirname, 'views'),  // set templates location
     extname: '.liquid'  // set file extension
@@ -12,8 +14,24 @@ app.engine('liquid', engine.express()); // register liquid engine
 app.set('views', path.resolve(__dirname, 'views')); // set views location
 app.set('view engine', 'liquid'); // set liquid as the view engine
 
-app.get('/', function (req, res) {
-    res.render('index', { message: 'Heeeellllo World' });
+// Serveer statische bestanden uit de 'static' map
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', async function (req, res) {
+    res.render('index', { headerText: 'Kleding Matcher'});
+    console.log(req);
+});
+
+app.get('/:kledingType', (req, res) => {
+    const kledingType = req.params.kledingType;
+    console.log(kledingType);
+    // Voer hier logica uit om de informatie over het gevraagde kledingType op te halen
+    res.send(`Informatie over kledingtype: ${kledingType}`);
+  });
+
+app.get('/kleding', async function (req, res) {
+    const clothesData = await getClothesData('kledingDetails');
+    res.render('kleding', { clothes: clothesData });
 });
 
 app.listen(5500);
