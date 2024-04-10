cdc904d5cf895b75ab51623a38281eb72e3d9701
maxstrikkers <maxstrikkers@outlook.com>
2024-04-10 13:24:58 +0200

    .

--- functies.js
+++ functies.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 
 async function getClothesData(fileName) {
-    const dataUrl = '/public/data/' + fileName + '.json';
+    const dataUrl = './public/data/' + fileName + '.json';
     const data = await fs.readFile(dataUrl, 'utf-8');
     return JSON.parse(data);
 }
